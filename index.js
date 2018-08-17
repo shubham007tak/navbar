ea5efc7446edb768167c387e5449245bda02fc27
unknown <shubham007tak>
2018-08-17 17:50:37 +0530

    navbar

---
+++ index.js
@@ -0,0 +1,47 @@
+$("document").ready(() =>{
+
+	$(".menus,.menus1").hover(function(){
+		$(this).css({"background-color":"#ddd","font-size":"30px"});
+	},
+	function(){
+		$(this).css({"background-color":"#333","font-size":"25px"});
+	});	
+
+	$(".menus,.menus1").hover(function(){
+		$(this).css({"background-color":"#ddd","font-size":"30px"});
+	},
+	function(){
+		$(this).css({"background-color":"#333","font-size":"25px"});
+	});
+
+	$(".menus,,.menus1").hover(function(){
+		$(this).css({"background-color":"#ddd","font-size":"30px"});
+	},
+	function(){
+		$(this).css({"background-color":"#333","font-size":"25px"});
+	});
+
+	$(".menus,.menus1").hover(function(){
+		$(this).css({"background-color":"#ddd","font-size":"30px"});
+	},
+	function(){
+		$(this).css({"background-color":"#333","font-size":"25px"});
+	});
+
+});
+
+
+
+
+
+$(document).ready(function(){
+
+	$(".navbar1").hide();
+    
+    $("#aid").click(function(){
+        $(".navbar1").toggle(100);
+    });
+});
+
+
+
